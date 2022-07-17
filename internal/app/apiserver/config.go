96b6ab9869a4e85086fe11ddf5517b53e733e633
Arima Takao <anno.raiden@protonmail.com>
2022-07-17 14:13:10 +0300

    Added secret_key

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,15 +1,17 @@
 package apiserver
 
 type Config struct {
-	BindAddr string `toml:"bind_addr"`
-	LogLevel string `toml:"log_lvl"`
-	DBURI    string `toml:"db_uri"`
+	BindAddr  string `toml:"bind_addr"`
+	LogLevel  string `toml:"log_lvl"`
+	DBURI     string `toml:"db_uri"`
+	SecretKey []byte `toml secret_key`
 }
 
 func NewConfig() *Config {
 	return &Config{
-		BindAddr: ":8000",
-		LogLevel: "debug",
-		DBURI:    "",
+		BindAddr:  ":8000",
+		LogLevel:  "debug",
+		DBURI:     "",
+		SecretKey: []byte("1111"),
 	}
 }
