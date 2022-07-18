18503ac137935c40fb7188f16286ce504abcb7a7
Arima Takao <anno.raiden@protonmail.com>
2022-07-18 13:15:06 +0300

    Implement handleDeleteUser

--- internal/app/apiserver/routes.go
+++ internal/app/apiserver/routes.go
@@ -83,8 +83,32 @@ func (srv *APIServer) handleSignin() http.HandlerFunc {
 }
 
 func (srv *APIServer) handleDeleteUser() http.HandlerFunc {
+	type request struct {
+		Name     string `json:"name"`
+		Password string `json:"password"`
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Delete user")
+		var req request
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			io.WriteString(w, `{ "error" : "Wrong json" }`)
+			return
+		}
+		hash, err := bcrypt.GenerateFromPassword([]byte(req.Password), 0)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			io.WriteString(w, `{ "error" : "Something wrong" }`)
+			return
+		}
+		countDeleted := srv.db.DeleteUser(req.Name, string(hash))
+		if countDeleted < 1 {
+			w.WriteHeader(http.StatusBadRequest)
+			io.WriteString(w, `{ "error" : "Something wrong" }`)
+			return
+		}
+		io.WriteString(w, `{ "message": "Deleted is success" }`)
 	}
 }
 
