5f3844510b74a405c43a6f07099e2561d53ec19e
Arima Takao <anno.raiden@protonmail.com>
2022-07-20 16:43:44 +0300

    Edit tag

--- internal/app/database/models.go
+++ internal/app/database/models.go
@@ -7,7 +7,7 @@ import (
 type GroupLinks struct {
 	Id          primitive.ObjectID `json:"_id" bson:"_id"`
 	IdOwner     primitive.ObjectID `json:"id_owner" bson:"id_owner"`
-	Title       string             `json:"title,omitempty" bson:"name,omitempty"`
+	Title       string             `json:"title,omitempty" bson:"title,omitempty"`
 	Tags        []string           `json:"tags,omitempty" bson:"tags,omitempty"`
 	Description string             `json:"description,omitempty" bson:"description,omitempty"`
 	Links       []string           `json:"links,omitempty" bson:"links,omitempty"`
