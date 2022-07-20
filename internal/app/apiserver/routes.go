2e8a4ce0fc4eec325242eeff5d93446550afd317
Arima Takao <anno.raiden@protonmail.com>
2022-07-20 16:45:17 +0300

    Implement all routers

--- internal/app/apiserver/routes.go
+++ internal/app/apiserver/routes.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/arimatakao/mycoll-api/internal/app/database"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -21,7 +22,7 @@ func (srv *APIServer) handleHello() http.HandlerFunc {
 			Apiname:    "mycoll",
 			Version:    "v1",
 			Countusers: srv.db.CountUsers(),
-			Countlinks: srv.db.CountLinks(),
+			Countlinks: srv.db.CountGroupLinks(),
 		}
 		json.NewEncoder(w).Encode(&resp)
 	}
@@ -104,13 +105,46 @@ func (srv *APIServer) handleSignin() http.HandlerFunc {
 }
 
 func (srv *APIServer) handleDeleteUser() http.HandlerFunc {
-	type request struct {
-		Name     string `json:"name"`
-		Password string `json:"password"`
-	}
-
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		id, valid := srv.parseTokenFromHeader(r)
+		if !valid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		countDeleted := srv.db.DeleteUser(id)
+		if countDeleted < 1 {
+			w.WriteHeader(http.StatusBadRequest)
+			io.WriteString(w, `{ "error": "Something wrong" }`)
+			return
+		}
+
+		countDeletedLinks := srv.db.DeleteAllGroupLinksByIdOwner(id)
+
+		srv.logger.Info("One user was deleted")
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"massage":             "Success",
+			"count_deleted_links": countDeletedLinks,
+		})
+	}
+}
+
+func (srv *APIServer) handleCreateLinks() http.HandlerFunc {
+	type request struct {
+		Title       string   `json:"title"`
+		Tags        []string `json:"tags"`
+		Description string   `json:"description"`
+		Links       []string `json:"links"`
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		id, valid := srv.parseTokenFromHeader(r)
+		if !valid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
 		var req request
 		err := json.NewDecoder(r.Body).Decode(&req)
 		if err != nil {
@@ -118,50 +152,90 @@ func (srv *APIServer) handleDeleteUser() http.HandlerFunc {
 			io.WriteString(w, `{ "error": "Wrong json" }`)
 			return
 		}
-		dbname, dbpassword := srv.db.GetUserNamePassword(req.Name)
-		if dbname != req.Name {
-			w.WriteHeader(http.StatusBadRequest)
-			io.WriteString(w, `{ "error": "Wrong username or password" }`)
-			return
-		}
-		err = bcrypt.CompareHashAndPassword([]byte(dbpassword), []byte(req.Password))
+		resultid, err := srv.db.CreateGroupLinks(id, req.Title, req.Tags, req.Description, req.Links)
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			io.WriteString(w, `{ "error": "Wrong username or password" }`)
-			return
-		}
-
-		countDeleted := srv.db.DeleteUser(req.Name)
-		if countDeleted < 1 {
-			w.WriteHeader(http.StatusBadRequest)
 			io.WriteString(w, `{ "error": "Something wrong" }`)
-			return
 		}
-		io.WriteString(w, `{ "message": "Deleted is success" }`)
-		srv.logger.Info("One user was deleted")
-	}
-}
-
-func (srv *APIServer) handleCreateLinks() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Create link")
+		srv.logger.Info("Created new group links")
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"massage": resultid,
+		})
 	}
 }
 
 func (srv *APIServer) handleFindLinks() http.HandlerFunc {
+	type response struct {
+		Links []database.GroupLinks `json:"links"`
+	}
 	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Find links")
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		id, ok := srv.parseTokenFromHeader(r)
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		result := srv.db.FindAllGroupLinksByIdOwner(id)
+		resp := response{
+			Links: result,
+		}
+		srv.logger.Info("Find group links")
+		json.NewEncoder(w).Encode(resp)
 	}
 }
 
 func (srv *APIServer) handleUpdateLinks() http.HandlerFunc {
+	type request struct {
+		Id          string   `json:"_id"`
+		Title       string   `json:"title"`
+		Tags        []string `json:"tags"`
+		Description string   `json:"description"`
+		Links       []string `json:"links"`
+	}
 	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Update links")
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		_, ok := srv.parseTokenFromHeader(r)
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		var req request
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			io.WriteString(w, `{ "error": "Wrong json" }`)
+			return
+		}
+		srv.logger.Info("Updated links")
+		countedUpdated := srv.db.UpdateGroupLinksById(req.Id, req.Title, req.Tags, req.Description, req.Links)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"message":       "Update success",
+			"count_updated": countedUpdated,
+		})
 	}
 }
 
 func (srv *APIServer) handleDeleteLinks() http.HandlerFunc {
+	type request struct {
+		Id string `json:"_id"`
+	}
 	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Delete links")
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		_, ok := srv.parseTokenFromHeader(r)
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		var req request
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			io.WriteString(w, `{ "error": "Wrong json" }`)
+			return
+		}
+		srv.logger.Info("Deleted links")
+		countDeleted := srv.db.DeleteGroupLinksById(req.Id)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"count_deleted_links": countDeleted,
+		})
 	}
 }
